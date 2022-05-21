05311943314043b445b11d670e0cba66ae978b83
Ruslan Shakirov <shakirov3000@mail.ru>
2022-05-21 15:53:27 +0500

    homework 1 added

--- Classes_1.py
+++ Classes_1.py
@@ -21,15 +21,15 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
- 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+alex = Lecturer('alex', 'ivanov') 
+print(alex.surname)
+print(alex.rate_hw)
